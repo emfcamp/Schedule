7e35f62e81f2baf88b45aa8b88f989bb554f9c8d
Alex Pounds <alex+git@alexpounds.com>
2014-08-26 01:02:28 +0100

    Display stars to logged-in users in the template; provide routable actions in the view for favouriting and unfavouriting events.

--- views/events.py
+++ views/events.py
@@ -1,8 +1,9 @@
 from main import app, db
 from views import Form
-from models.event import Event
+from models.event import Event, EventFavourite
 
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import joinedload
 
 from flask import (
     render_template, redirect, request, flash,
@@ -12,12 +13,35 @@ from flask.ext.login import (
     login_user, login_required, logout_user, current_user,
 )
 
+from wtforms.validators import Required
+from wtforms import TextField, PasswordField, HiddenField
+
 import re
+
+class EventFavouriteForm(Form):
+    pass
+
 
 @app.route('/events', methods=['GET'])
 def events():
+    form = EventFavouriteForm()
     events = Event.query.order_by(Event.location_id, Event.start_time).all()
-    return render_template("events.html", events=events)
+    return render_template("events.html", events=events, form=form)
 
+@app.route('/events/<id>/favourite', methods=['POST'])
+@login_required
+def add_favourite(id):
+    event = Event.query.get(id)
+    if event is not None and event not in current_user.events:
+        current_user.events.append(event)
+        db.session.commit()
+    return redirect(url_for('events'))
 
-
+@app.route('/events/<id>/unfavourite', methods=['POST'])
+@login_required
+def remove_favourite(id):
+    event = Event.query.get(id)
+    if event is not None and event in current_user.events:
+        EventFavourite.query.filter_by(event_id=event.id, user_id=current_user.id).delete()
+        db.session.commit()
+    return redirect(url_for('events'))
